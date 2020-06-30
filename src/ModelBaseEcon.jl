d2053e14295d0a15c100cc3d3b41b15613ebeacc
Boyan Bejanov <bbejanov@gmail.com>
2020-06-30 16:59:58 -0400

    Added @using_example

--- src/ModelBaseEcon.jl
+++ src/ModelBaseEcon.jl
@@ -32,4 +32,30 @@ include("equation.jl")
 include("steadystate.jl")
 include("model.jl")
 
+
+"""
+    @using_example name
+
+Load models from the package examples/ folder.
+The `@load_example` version is deprecated - stop using it now.
+"""
+macro using_example(name)
+    examples_path = joinpath(dirname(pathof(@__MODULE__)), "..", "examples")
+    return quote
+        push!(LOAD_PATH, $(examples_path))
+        using $(name)
+        pop!(LOAD_PATH)
+        $(name)
+    end |> esc
+end
+
+" Deprecated. Use `@using_example` instead."
+macro load_example(name)
+    Base.depwarn("Use `@using_example` instead.", Symbol("@load_example"))
+    return esc(:(@using_example $name))
+end
+export @using_example, @load_example
+
+
+
 end # module
