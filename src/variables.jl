0263355afcb0b4bff002b935930f2da21ec123e8
Boyan Bejanov <bbejanov@gmail.com>
2022-03-08 15:10:23 -0500

    split var_type into 3 fields

--- src/variables.jl
+++ src/variables.jl
@@ -13,33 +13,91 @@ const doc_macro = MacroTools.unblock(quote
     world
 end).args[1]
 
+const variable_types = (:var, :shock, :exog)
+const transformation_types = (:none, :log, :neglog)
+const steadystate_types = (:const, :growth)
 
-struct ModelVariable{T <: Transformation}
+struct ModelVariable
     doc::String
     name::Symbol
-    var_type::Symbol
+    vr_type::Symbol   # one of :var, :shock, :exog
+    tr_type::Symbol    # transformation, one of :none, :log, :neglog
+    ss_type::Symbol    # behaviour as t → ∞, one of :const, :growth
     index::Int
+    ModelVariable(d, n, vt, tt, st, ind) = begin
+        vt ∈ variable_types || error("Unknown variable type $vt. Expected one of $variable_types")
+        tt ∈ transformation_types || error("Unknown transformation type $tt. Expected one of $transformation_types")
+        st ∈ steadystate_types || error("Unknown steady state type $st. Expected one of $steadystate_types")
+        new(d, n, vt, tt, st, ind)
+    end
+end
+
+function Base.getproperty(v::ModelVariable, s::Symbol)
+    if s === :var_type
+        vt = getfield(v, :vr_type)
+        if vt === :shock || vt === :exog
+            return vt
+        end
+        tt = getfield(v, :tr_type)
+        if tt !== :none 
+            return tt
+        end
+        if getfield(v, :ss_type) === :const
+            return :steady
+        end
+        return :lin
+    end
+    return getfield(v, s)
 end
 
 function ModelVariable(d, s, t)
-    T = ifelse(t == :log, LogTransform, ifelse(t == :neglog, NegLogTransform, NoTransform))
-    return ModelVariable{T}(d, s, t, -1)
+    if t ∈ (:log, :neglog)
+        return ModelVariable(d, s, :var, t, :growth, -1)
+    elseif t === :steady
+        return ModelVariable(d, s, :var, :none, :const, -1)
+    elseif t == :lin
+        return ModelVariable(d, s, :var, :none, :growth, -1)
+    elseif t ∈ (:shock, :exog)
+        return ModelVariable(d, s, t, :none, :growth, -1)
+    end
+    # T = ifelse(t == :log, LogTransform, ifelse(t == :neglog, NegLogTransform, NoTransform))
 end
+
+_sym2trans(s::Symbol) = _sym2trans(Val(s))
+_sym2trans(::Val) = NoTransform
+_sym2trans(::Val{:log}) = LogTransform
+_sym2trans(::Val{:neglog}) = NegLogTransform
+
+_trans2sym(::Type{NoTransform}) = :none
+_trans2sym(::Type{LogTransform}) = :log
+_trans2sym(::Type{NegLogTransform}) = :neglog
 
 # for compatibility with old code. will be removed soon.
 const ModelSymbol = ModelVariable
 
 # !!! must not update v.name.
-@inline update(v::ModelVariable{T};
-    doc=v.doc,
-    var_type=v.var_type,
-    index=v.index,
-    transformation::Type{<:Transformation}=T,
-) where {T <: Transformation} = ModelVariable{transformation}(string(doc), v.name, Symbol(var_type), Int(index))
+function update(v::ModelVariable; doc = v.doc,
+    vr_type::Symbol = v.vr_type, tr_type::Symbol = v.tr_type, ss_type::Symbol = v.ss_type,
+    index = v.index, transformation = nothing)
+    if transformation !== nothing
+        trsym = _trans2sym(transformation)
+        if (tr_type == v.tr_type)
+            # only transformation is explicitly given
+            tr_type = trsym
+        elseif (tr_type == trsym)
+            # both given and they match
+            tr_type = trsym
+        else
+            # both given and don't match
+            error("Given `transformation` is incompatible with the given `ss_type`.")
+        end
+    end
+    ModelVariable(string(doc), v.name, vr_type, tr_type, ss_type, Int(index))
+end
 
-@inline ModelVariable(s::Symbol) = ModelVariable("", s, :lin)
-@inline ModelVariable(d::String, s::Symbol) = ModelVariable(d, s, :lin)
-@inline ModelVariable(s::Symbol, t::Symbol) = ModelVariable("", s, t)
+ModelVariable(s::Symbol) = ModelVariable("", s, :var, :none, :growth, -1)
+ModelVariable(d::String, s::Symbol) = ModelVariable(d, s, :var, :none, :growth, -1)
+ModelVariable(s::Symbol, t::Symbol) = ModelVariable("", s, t)
 
 function ModelVariable(s::Expr)
     s = MacroTools.unblock(s)
@@ -60,19 +118,20 @@ function ModelVariable(doc::String, s::Expr)
     end
 end
 
-for sym ∈ (:shock, :log, :neglog, :lin, :steady, :exog)
-    to_sym = Symbol("to_", sym)
-    issym = Symbol("is", sym)
-    eval(quote
-        @inline $(to_sym)(s::ModelVariable) = ModelVariable(s.doc, s.name, $(QuoteNode(sym)))
-        @inline $(to_sym)(any) = $(to_sym)(convert(ModelVariable, any))
-        export $(to_sym)
-        @inline $(issym)(any) = false
-        @inline $(issym)(s::ModelVariable) = s.var_type == $(QuoteNode(sym))
-        export $(issym)
-    end)
-end
-
+to_shock(v) = update(convert(ModelVariable, v); vr_type = :shock)
+to_exog(v) = update(convert(ModelVariable, v); vr_type = :exog)
+to_steady(v) = update(convert(ModelVariable, v); ss_type = :const)
+to_lin(v) = update(convert(ModelVariable, v); tr_type = :none)
+to_log(v) = update(convert(ModelVariable, v); tr_type = :log)
+to_neglog(v) = update(convert(ModelVariable, v); tr_type = :neglog)
+isshock(v) = v.vr_type == :shock
+isexog(v) = v.vr_type == :exog
+issteady(v) = v.ss_type == :const
+islin(v) = v.tr_type == :none
+islog(v) = v.tr_type == :log
+isneglog(v) = v.tr_type == :neglog
+export to_shock, to_exog, to_steady, to_lin, to_log, to_neglog
+export isshock, isexog, issteady, islin, islog, isneglog
 
 Symbol(v::ModelVariable) = v.name
 Base.convert(::Type{Symbol}, v::ModelVariable) = v.name
@@ -102,12 +161,12 @@ end
 
 export transform, inverse_transform
 
-@inline transformation(::ModelVariable{T}) where {T <: Transformation} = transformation(T)
-@inline inverse_transformation(::ModelVariable{T}) where {T <: Transformation} = inverse_transformation(T)
+transformation(v::ModelVariable) = transformation(_sym2trans(v.tr_type))
+inverse_transformation(v::ModelVariable) = inverse_transformation(_sym2trans(v.tr_type))
 
 # redirect to the stored transform
 transform(x, m::ModelVariable) = broadcast(transformation(m), x)
 inverse_transform(x, m::ModelVariable) = broadcast(inverse_transformation(m), x)
 
 need_transform(a) = need_transform(convert(ModelVariable, a))
-need_transform(::ModelVariable{T}) where {T <: Transformation} = T != NoTransform
+need_transform(v::ModelVariable) = _sym2trans(v.tr_type) != NoTransform
